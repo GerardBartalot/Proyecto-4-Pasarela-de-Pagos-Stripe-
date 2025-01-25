9647585697797ade9e79057cdf97c8910332d923
Gerard <gerard@example.com>
2025-01-25 14:05:32 +0100

    Creación Endpoint User realizado

--- src/main/java/com/stripe/stripe_payments/services/impl/StripeServiceImpl.java
+++ src/main/java/com/stripe/stripe_payments/services/impl/StripeServiceImpl.java
@@ -1,8 +1,16 @@
 package com.stripe.stripe_payments.services.impl;
 
+import com.stripe.Stripe;
 import com.stripe.exception.SignatureVerificationException;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
 import com.stripe.model.Event;
+import com.stripe.model.Price;
+import com.stripe.model.Product;
 import com.stripe.net.Webhook;
+import com.stripe.param.CustomerCreateParams;
+import com.stripe.param.PriceCreateParams;
+import com.stripe.param.ProductCreateParams;
 import com.stripe.stripe_payments.services.StripeService;
 import com.stripe.stripe_payments.strategy.StripeStrategy;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,7 +25,9 @@ public class StripeServiceImpl implements StripeService {
     private final String endpointSecret;
     private final List<StripeStrategy> stripeStrategies;
 
-    public StripeServiceImpl(@Value("${stripe.endpoint.secret}") String endpointSecret, List<StripeStrategy> stripeStrategies) {
+    public StripeServiceImpl(@Value("${stripe.endpoint.secret}") String endpointSecret, List<StripeStrategy> stripeStrategies,
+                             @Value("${stripe.secret.key}") String stripeKey) {
+        Stripe.apiKey = stripeKey;
         this.endpointSecret = endpointSecret;
         this.stripeStrategies = Collections.unmodifiableList(stripeStrategies);
     }
@@ -59,6 +69,48 @@ public class StripeServiceImpl implements StripeService {
             return event;
         } catch (SignatureVerificationException e) {
             System.err.println("Error verificando la firma del webhook: " + e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Customer createCustomer(String email) {
+        var customerCreateParams = CustomerCreateParams.builder()
+                .setEmail(email)
+                .build();
+
+        try {
+            return Customer.create(customerCreateParams);
+        } catch (StripeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Product createProduct(String name) {
+        var productCreateParams = ProductCreateParams.builder()
+                .setName(name)
+                .setType(ProductCreateParams.Type.SERVICE)
+                .build();
+
+        try {
+            return Product.create(productCreateParams);
+        } catch (StripeException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public Price createPrice(String productId) {
+        var createPrice = PriceCreateParams.builder()
+                .setCurrency("eur")
+                .setProduct(productId)
+                .setUnitAmount(4000L)
+                .build();
+
+        try {
+            return Price.create(createPrice);
+        } catch (StripeException e) {
             throw new RuntimeException(e);
         }
     }
